514d6fa228fdf596bf1215c4db3e090ee36b264b
tepythai <thaitepy@gmail.com>
2023-03-18 15:35:45 +0900

    add astro image

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import node from '@astrojs/node';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
@@ -11,7 +12,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   // Enable Solid to support Solid JSX components.
-  integrations: [solid(), tailwind(), react()],
+  integrations: [solid(), tailwind(), react(), image()],
   output: 'server',
   adapter: node({
     mode: 'middleware',
